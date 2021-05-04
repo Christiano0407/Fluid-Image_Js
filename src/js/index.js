9c06e12227b234351d42fbd8892bce4b705748f0
Christiano0407 <placebo0704@gmail.com>
2021-05-04 09:58:25 -0500

    one-part

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector(".modal"); 
+const previews = document.querySelectorAll(".gallery img"); 
+const original = document.querySelector(".full-img");
+const imgText = document.querySelector(".caption"); 
+
+previews.forEach((preview) => {
+    preview.addEventListener("click", () => {
+        modal.classList.add("open");
+        original.classList.add("open"); 
+    });
+});
+
+console.log("One"); 
+
+modal.addEventListener("click", (e) => {
+    if(e.target.classList.contains("modal")) {
+        modal.classList.remove("open"); 
+    }
+});
+

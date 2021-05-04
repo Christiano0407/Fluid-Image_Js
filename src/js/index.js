c2f2789ed20308e11df1f0874a6143dc380c3061
Christiano0407 <placebo0704@gmail.com>
2021-05-04 11:48:02 -0500

    fluid-image

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
 const modal = document.querySelector(".modal"); 
 const previews = document.querySelectorAll(".gallery img"); 
 const original = document.querySelector(".full-img");
-const imgText = document.querySelector(".caption"); 
+const caption = document.querySelector(".caption"); 
 
 previews.forEach((preview) => {
     preview.addEventListener("click", () => {
         modal.classList.add("open");
         original.classList.add("open"); 
+        // Dynamic Change text and image
+       const originalSrc = preview.getAttribute("data-original"); 
+       original.src = `../assets/image/${originalSrc}`;
+       const altText = preview.alt; 
+       caption.textContent = altText; 
     });
 });
 
@@ -15,6 +20,7 @@ console.log("One");
 modal.addEventListener("click", (e) => {
     if(e.target.classList.contains("modal")) {
         modal.classList.remove("open"); 
+        original.classList.remove("open"); 
     }
 });
 
